6ab31a0d10e23892b11c338d07bb74242b089856
alvaro.lopez.cruz <alvaro.lopez.cruz@alumnos.upm.es>
2020-05-23 17:35:36 +0200

    Basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
