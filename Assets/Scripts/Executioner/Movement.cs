da5e634513bec5c6f198dfc65d846e0a6339cfda
alvaro.lopez.cruz <alvaro.lopez.cruz@alumnos.upm.es>
2020-06-02 22:15:56 +0200

    Added enemy sprite & movement script issues #6 #7

---
+++ Assets/Scripts/Executioner/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float movespeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * movespeed * Time.deltaTime));
+    }
+
+}
